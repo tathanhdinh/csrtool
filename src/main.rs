2113b3200c99d7ea0cc901d8b1d7915b21f905c3
Ta Thanh Dinh <tathanhdinh@gmail.com>
2017-04-12 00:48:38 +0200

    continue

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 extern crate capstone;
 
+#[macro_use] extern crate prettytable;
+
 arg_enum!{
     #[allow(non_camel_case_types)]
     enum AddressingModes {
@@ -41,45 +43,50 @@ fn main() {
     let default_architecture = Architectures::x86.to_string();
     let default_assembly_syntax = AssemblySyntax::intel.to_string();
     let default_base_address = 0usize.to_string();
-    let default_physical_offset = 0usize.to_string();
+    let default_file_offset = 0usize.to_string();
 
-    let matches = clap::App::new("csrtool")
-        .version("0.1")
-        .author("Ta Thanh Dinh <tathanhdinh@gmail.com>")
-        .about("an improved cstool")
+    let matches = clap::App::new("csrtool - an improved objdump (Ta Thanh Dinh <tathanhdinh@gmail.com>)")
+        // .version("0.1")
+        // .author("Ta Thanh Dinh <tathanhdinh@gmail.com>")
+        // .about("an improved cstool")
         .arg(clap::Arg::with_name("mode")
              .short("m")
              .long("mode")
              .help("Addressing mode")
+             .takes_value(true)
              .possible_values(&AddressingModes::variants())
              .default_value(default_addressing_mode.as_str())
              .required(false))
         .arg(clap::Arg::with_name("arch")
              .long("arch")
              .help("CPU architecture")
-             .required(true)
+             .takes_value(true)
              .possible_values(&Architectures::variants())
              .default_value(default_architecture.as_str())
              .required(false))
         .arg(clap::Arg::with_name("syntax")
              .long("syntax")
              .help("Assembly syntax")
+             .takes_value(true)
              .possible_values(&AssemblySyntax::variants())
              .default_value(default_assembly_syntax.as_str())
              .required(false))
-        .arg(clap::Arg::with_name("physical offset")
+        .arg(clap::Arg::with_name("file offset")
              .long("offset")
-             .help("Start disassembling from a physical offset")
-             .default_value(default_physical_offset.as_str())
+             .takes_value(true)
+             .help("Start disassembling from a file offset")
+             .default_value(default_file_offset.as_str())
              .required(false))
         .arg(clap::Arg::with_name("length")
              .short("l")
              .long("length")
+             .takes_value(true)
              .help("Number of disassembled bytes (or disassembling until EOF)")
              .required(false))
         .arg(clap::Arg::with_name("base address")
              .long("base")
              .help("Base address of disassembled instructions")
+             .takes_value(true)
              .default_value(default_base_address.as_str())
              .required(false))
         .arg(clap::Arg::with_name("smc")
@@ -90,13 +97,13 @@ fn main() {
              .short("f")
              .long("file")
              .help("Input file")
+             .takes_value(true)
              .required(true))
         .get_matches();
 
     // let mode = matches.value_of("mode").unwrap_or_default();
     // match matches.value_of("file") {
     //     Some(file) => {
-            
     //     },
     //     _ => {
     //         println!("unknown input file");
@@ -104,11 +111,11 @@ fn main() {
     // }
 
     let input_filename = value_t!(matches.value_of("file"), String).unwrap_or_else(|e| e.exit());
+    // let input_filename = matches.value_of("file").unwrap_or_else(|e| { println!("{}", e); std::process::exit(1); });
 
     let mode = value_t!(matches.value_of("mode"), AddressingModes).unwrap_or_else(|e| e.exit());
     let arch = value_t!(matches.value_of("arch"), Architectures).unwrap_or_else(|e| e.exit());
     let disasm_syntax = value_t!(matches.value_of("syntax"), AssemblySyntax).unwrap_or_else(|e| e.exit());
-    let base_address = value_t!(matches.value_of("base"), usize).unwrap_or_else(|e| e.exit());
 
     match mode {
         AddressingModes::x32 => {
@@ -116,13 +123,12 @@ fn main() {
                 Architectures::x86 => {
                     match disasm_syntax {
                         AssemblySyntax::intel => {
-                            let poff = value_t!(matches.value_of("base address"), usize).unwrap_or_else(|e| e.exit());
+                            let poff = value_t!(matches.value_of("file offset"), usize).unwrap_or_else(|e| e.exit());
                             // let file_metadata = std::fs::metadata(input_filename.as_str()).unwrap_or_else(|e| println!("{}", e));
                             let file_metadata = std::fs::metadata(input_filename.as_str())
                                 .unwrap_or_else(|e| { println!("{}", e); std::process::exit(1); });
+
                             if file_metadata.is_file() {
-                                println!("{} is not a valid file", input_filename);
-                            } else {
                                 let file_length = file_metadata.len() as usize;
 
                                 if poff < file_length {
@@ -147,6 +153,31 @@ fn main() {
                                         .unwrap_or_else(|e| { println!("{}", e); std::process::exit(1); });
                                     buffer.truncate(nb_read);
 
+                                    let base_address = value_t!(matches.value_of("base address"), usize)
+                                        .unwrap_or_else(|e| e.exit());
+
+                                    let cs = capstone::Capstone::new(capstone::CsArch::ARCH_X86, capstone::CsMode::MODE_32)
+                                        .unwrap_or_else(|e| { println!("{}", e); std::process::exit(1); });
+
+                                    let insns = cs.disasm(&buffer, base_address as u64, 0)
+                                        .unwrap_or_else(|e| { println!("{}", e); std::process::exit(1); });
+
+                                    let mut table = prettytable::Table::new();
+                                    table.set_format(*prettytable::format::consts::FORMAT_CLEAN);
+
+                                    for ins in insns.iter() {
+                                        // println!("{}", ins);
+                                        let mnemonic = ins.mnemonic().unwrap_or("");
+                                        let op = ins.op_str().unwrap_or("");
+                                        let mnemonic_op_str = format!("{} {}", mnemonic, op);
+                                        let addr_str = format!("{:#x}", ins.address);
+
+                                        table.add_row(row![addr_str, mnemonic_op_str]);
+                                        // println!("{:<#10x}  {} {}", ins.address, mnemonic, op);
+                                    }
+
+                                    table.printstd();
+
                                     // match std::fs::File::open(input_filename.as_str()) {
                                     //     Ok(mut input_file) => {
                                     //         use std::io::Seek;
@@ -160,6 +191,8 @@ fn main() {
                                 } else {
                                     println!("offset is too large")
                                 }
+                            } else {
+                                println!("{} is not a valid file", input_filename);
                             }
 
                             // match std::fs::metadata(input_filename.as_str()) {
